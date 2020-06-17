6aec56ebc0f1c5b24b093a1f182756aada1b496d
Harry Dave <hdave014@gmail.com>
2020-06-18 05:18:30 +1200

    Add project files.

---
+++ src/app/CV.POS.Wpf/View/Login/MainLogin.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+
+namespace CV.POS.Wpf.View.Login
+{
+    /// <summary>
+    /// Interaction logic for MainLogin.xaml
+    /// </summary>
+    public partial class MainLogin : UserControl
+    {
+        public MainLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginPart_Loaded()
+        {
+
+        }
+    }
+}
